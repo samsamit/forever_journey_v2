1665fd6b4a98a548be239e9482ea57be7d040783
Samu Tiainen <samu.tiainen@hotmail.com>
2021-05-11 19:54:35 +0300

    Added "Make me admin button"

--- src/components/user/UserCard.tsx
+++ src/components/user/UserCard.tsx
@@ -1,10 +1,16 @@
-import classes from "*.module.css";
-import { makeStyles } from "@material-ui/core";
+import { useMutation } from "@apollo/client";
+import { Button, makeStyles } from "@material-ui/core";
 import Card from "@material-ui/core/Card";
-import React from "react";
-import { useSelector } from "react-redux";
-import { IUserState } from "../../GlobalState/Reducers/UserReducer";
+import { useSnackbar } from "notistack";
+import { useDispatch, useSelector } from "react-redux";
+import { UPDATE_USER } from "../../controllers/user/userController";
+import { LOGOUT_USER } from "../../GlobalState/Reducers/UserReducer";
 import { IRootState } from "../../GlobalState/store";
+import { UserRole } from "../../types/globalTypes";
+import {
+  updateUser_updateUser,
+  updateUserVariables,
+} from "../../types/updateUser";
 const useStyle = makeStyles({
   root: {
     margin: 20,
@@ -14,10 +20,37 @@ const useStyle = makeStyles({
 interface IProps {}
 export const UserCard = (props: IProps) => {
   const user = useSelector((state: IRootState) => state.user);
+  const { enqueueSnackbar } = useSnackbar();
   const classes = useStyle();
+  const dispatch = useDispatch();
+  const [editUser, { loading, error }] = useMutation<
+    updateUser_updateUser,
+    updateUserVariables
+  >(UPDATE_USER);
+
+  const makeAdmin = async () => {
+    await editUser({
+      variables: {
+        patch: {
+          filter: { username: { eq: user.userInfo?.username } },
+          set: {
+            role: UserRole.ADMIN,
+          },
+        },
+      },
+    });
+    dispatch({ type: LOGOUT_USER });
+    enqueueSnackbar("You are now admin! Please login again.", {
+      variant: "success",
+    });
+  };
+
   return (
     <Card className={classes.root}>
       {user.userInfo ? user.userInfo?.username : "Loading..."}
+      {user.userInfo?.role !== UserRole.ADMIN && (
+        <Button onClick={makeAdmin}>Make me ADMIN!</Button>
+      )}
     </Card>
   );
 };
